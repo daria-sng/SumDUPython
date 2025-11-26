f9692eb83200920e0977a03422d19def67c41b47
Daria Sng <darya.snagovskaya@gmail.com>
2025-11-26 16:32:58 +0200

    done

--- lab11.3.py
+++ lab11.3.py
@@ -30,12 +30,15 @@ plt.legend()
 
 plt.show()
 
-# Видалення стоп-слів та пунктуації з тексту
-stop_words = set(stopwords.words("english"))
-withoutstop = [word for word in hamlet if not word in stop_words]
+# Видалення стоп-слів ( з архаїчними словами ) та пунктуації з тексту
+stop_words = set(word.lower() for word in stopwords.words("english"))
+arch_words = {"thou", "thy", "thee", "ye"}
+stop_words.update(arch_words)
+
+withoutstop = [word for word in hamlet if word.lower() not in stop_words]
 
 table = str.maketrans("", "", string.punctuation)
-withoutpunctuation = [word.translate(table) for word in withoutstop]
+withoutpunctuation = [word.translate(table) for word in withoutstop if word.translate(table)!= ""]
 
 # Визначення 10 найбільш вживаних слів у тексті 
 fdist = FreqDist(withoutpunctuation)
